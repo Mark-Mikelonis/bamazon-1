a13a1ba7c25bf8b8d97064b3e812fddf9905fcbd
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-02-23 07:34:27 -0800

    add decimals

--- bamazon.sql
+++ bamazon.sql
@@ -5,8 +5,8 @@ USE bamazon_db;
 
 CREATE TABLE products (
     item_id INTEGER AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR(30) NOT NULL,
-    department_name VARCHAR(30),
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100),
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INTEGER(10) NOT NULL,
     product_sales DECIMAL (10,2) DEFAULT 0.00,
@@ -29,7 +29,7 @@ CREATE TABLE products (
 
     CREATE TABLE departments (
         department_id INTEGER (10) AUTO_INCREMENT NOT NULL,
-        department_name VARCHAR (50) NOT NULL,
+        department_name VARCHAR (100) NOT NULL,
         over_head_costs DECIMAL (10,2),
         PRIMARY KEY (department_id)    
     );
