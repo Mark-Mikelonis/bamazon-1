768b446131a97eb4ef44084c6f4ef813bc55a414
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-02-23 12:47:19 -0800

    major typos fixed

--- bamazon.sql
+++ bamazon.sql
@@ -14,16 +14,34 @@ CREATE TABLE products (
 );
 
     INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
-    VALUES  ('Baseball bat', 'Sporting Goods','149.99','11', '0.00'),
-            ('Gazebo', 'Outdoor Furniture','210.99','18', '0.00'),
-            ('Glory Beenie Baby', 'Toys','9.99','17', '0.00'),
-            ('Sony HD Television', 'Electronics','309.99','8', '0.00'),
-            ('Thai Spices', 'Food','15.00','9', '0.00'),
-            ('Candyland', 'Toys','9.99','7', '0.00'),
-            ('Queenbed sheets', 'Bedroom','49.99','20', '0.00'),
-            ('Macbook Pro', 'Electronics', '1999.99','22', '0.00'),
-            ('James Patterson: Alex', 'Books','19.99','35', '0.00'),
-            ('Garth Brooks Greatest Hits', 'Music','49.99','19', '0.00');
+    VALUES  ('Baseball bat', 'Sporting Goods','149.99','11', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES  ('Gazebo', 'Outdoor Furniture','210.99','18', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES  ('Glory Beenie Baby', 'Toys','9.99','17', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES  ('Sony HD Television', 'Electronics','309.99','8', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES  ('Thai Spices', 'Food','15.00','9', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES  ('Candyland', 'Toys','9.99','7', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES  ('Queenbed sheets', 'Bedroom','49.99','20', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES ('Macbook Pro', 'Electronics', '1999.99','22', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES ('James Patterson: Alex', 'Books','19.99','35', '0.00');
+
+    INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+    VALUES ('Garth Brooks Greatest Hits', 'Music','49.99','19', '0.00');
 
             SELECT * FROM products
 
@@ -35,15 +53,29 @@ CREATE TABLE products (
     );
 
     INSERT INTO departments (department_name, over_head_costs, total_sales)
-    VALUES  ('Sporting Goods','80.00', '0.00'),
-            ('Outdoor Furniture','100.00', '0.00'),
-            ('Toys','150.00', '0.00'),
-            ('Books','80.00', '0.00'),
-            ('Music','30.00', '0.00'),
-            ('Electronics','145.00', '0.00'),
-            ('Bedroom','95.00', '0.00'),
-            ('Food','75.00', '0.00');
+    VALUES  ('Sporting Goods','80.00', '0.00');
 
-            SELECT * FROM departments
+    INSERT INTO departments (department_name, over_head_costs, total_sales)
+    VALUES  ('Outdoor Furniture','100.00', '0.00');
+
+    INSERT INTO departments (department_name, over_head_costs, total_sales)
+    VALUES  ('Toys','150.00', '0.00'),
+
+    INSERT INTO departments (department_name, over_head_costs, total_sales)
+    VALUES  ('Books','80.00', '0.00'),
+
+    INSERT INTO departments (department_name, over_head_costs, total_sales)
+    VALUES  ('Music','30.00', '0.00');
+
+    INSERT INTO departments (department_name, over_head_costs, total_sales)
+    VALUES ('Electronics','145.00', '0.00');
+
+    INSERT INTO departments (department_name, over_head_costs, total_sales)
+    VALUES ('Bedroom','95.00', '0.00');
+
+    INSERT INTO departments (department_name, over_head_costs, total_sales)
+    VALUES ('Food','75.00', '0.00');
+
+    SELECT * FROM departments
 
     
